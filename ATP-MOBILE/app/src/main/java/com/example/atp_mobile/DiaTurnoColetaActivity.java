fa1ab794fea08e7f17456da98f8ced93081d7639
Achilles <g.achilles2007@gmail.com>
2024-06-30 22:46:24 -0300

    commit 02

--- ATP-MOBILE/app/src/main/java/com/example/atp_mobile/DiaTurnoColetaActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.atp_mobile;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class DiaTurnoColetaActivity extends AppCompatActivity {
-}
--- ATP-MOBILE/app/src/main/java/com/example/atp_mobile/PontosColetaActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.atp_mobile;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class PontosColetaActivity extends AppCompatActivity {
-
-}
